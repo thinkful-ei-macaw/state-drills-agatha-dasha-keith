327ac5024ebfc1a5ccd5a3e10d3763d0136116d3
Dasha Akhten <dasha@Dashas-iMac.attlocal.net>
2020-03-04 14:08:00 -0800

    final version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import Hello from './HelloWorld/HelloWorld';
-import Bomb from './Bomb/Bomb';
-import RouletteGun from './RouletteGun/RouletteGun'
+import React from "react";
+import Hello from "./HelloWorld/HelloWorld";
+import Bomb from "./Bomb/Bomb";
+import Accordion from "./Accordion/Accordion";
+
+const sections = [
+  {
+    title: "Section 1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    title: "Section 2",
+    content:
+      "Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!"
+  },
+  {
+    title: "Section 3",
+    content:
+      "Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?"
+  }
+];
 
 function App() {
   return (
-    <main className='App'>
-      <div className='Hello'>
+    <main className="App">
+      <div className="Hello">
         <Hello />
       </div>
-      <div className='Bomb'>
+      <div className="Bomb">
         <Bomb />
       </div>
-      <div className='RouletteGun'>
-        <RouletteGun />
+      <div className="RouletteGun">{/*<RouletteGun />*/}</div>
+      <div className="Accordion">
+        <Accordion sections={sections} />
       </div>
     </main>
   );
--- src/RouletteGun/RouletteGun.js
+++ src/RouletteGun/RouletteGun.js
@@ -1,48 +1,38 @@
-import React from 'react';
-
+import React from "react";
 
 export default class RouletteGun extends React.Component {
+  static defaultProps = {
+    bulletInChamber: 8
+  };
 
-    static defaultProps = {
-        bulletInChamber: 8
-    }
-
-state = {
+  state = {
     chamber: null,
-    spinningTheChamber: false,
-}
+    spinningTheChamber: false
+  };
 
-compentWillUnmount() {
-    clearTimeout(this.timeout)
-}
+  compentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
 
-    handleClick = () => {this.setState({spinningTheChamber: true,})
-    this.timeout =setTimeout(() => {
-        const randomChamber = Math.ceil(Math.random() * 8)
+  handleClick = () => {
+    this.setState({ spinningTheChamber: true });
+    this.timeout = setTimeout(() => {
+      const randomChamber = Math.ceil(Math.random() * 8);
 
-        this.setState({
-            chamber: randomChamber,
-            spinningTheChamber: false,
-        })
-    }
-}
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+      this.setState({
+        chamber: randomChamber,
+        spinningTheChamber: false
+      });
+    });
+  };
+  //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 
-    render() {
-
-
-
-
-        return (
-            <div className='RouletteGun'>
-                
-                    <button onClick = {this.handleClick}>Pull The Trigger</button>
-                    <p>
-                    {result}
-                </p>
-
-
-            </div>
-    )
-    }
+  render() {
+    return (
+      <div className="RouletteGun">
+        <button onClick={this.handleClick}>Pull The Trigger</button>
+        <p>{result}</p>
+      </div>
+    );
+  }
 }
